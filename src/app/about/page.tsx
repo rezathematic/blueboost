85a27a84ef746e51330a1c73b689a61b476cbec7
Reza <r.bahar91@gmail.com>
2024-06-07 16:41:00 +0530

    changed about page hero image

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,16 @@
-import Hero from "@/app/components/hero";
-import TopServicesOverview from "@/app/components/top-services-overview";
-import AlgoCta from "@/app/components/algo-cta";
-import ServicesOverview from "@/app/components/services-overview";
-import StrategyCTA from "@/app/components/strategy-cta";
-import StrategyFrameworkTimeline from "@/app/components/strategic-framework-timeline";
-import CtaPanel from "@/app/components/cta-panel";
-import AdditionalSEOServices from "@/app/components/additional-seo-services";
-import Team from "@/app/components/team";
+import Hero from '@/app/components/hero'
+import TopServicesOverview from '@/app/components/top-services-overview'
+import AlgoCta from '@/app/components/algo-cta'
+import ServicesOverview from '@/app/components/services-overview'
+import StrategyCTA from '@/app/components/strategy-cta'
+import StrategyFrameworkTimeline from '@/app/components/strategic-framework-timeline'
+import CtaPanel from '@/app/components/cta-panel'
+import AdditionalSEOServices from '@/app/components/additional-seo-services'
+import Team from '@/app/components/team'
+import aboutImage from '../../../public/img/about-blue-boost-office.jpeg'
 
 // Lib imports
-import { topServices, servicesOverview } from "@/app/lib/services";
+import { topServices, servicesOverview } from '@/app/lib/services'
 
 export default function AboutPage() {
   return (
@@ -19,6 +20,8 @@ export default function AboutPage() {
         heading="An SEO Agency That Guarantees Results Or They Don't Get Paid."
         description="If you're wondering, we're perfectly sane and serious about this proposition. Blue Boost is a data-driven SEO agency dedicated to empowering our customers with search-first marketing services."
         hasButton={true}
+        image={aboutImage}
+        imageAlt="Blue Boost office"
       />
       <hr className="border-gray-200" />
       <Team />
@@ -34,5 +37,5 @@ export default function AboutPage() {
       <ServicesOverview services={servicesOverview} />
       <StrategyCTA />
     </>
-  );
+  )
 }
