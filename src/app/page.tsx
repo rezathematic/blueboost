552d8ee97af1b1ab1a2323c82c6836dce23f7ea8
Reza <r.bahar91@gmail.com>
2024-07-26 19:37:38 +0530

    Added Title and Desc for SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,29 @@
-import Hero from "@/app/hero";
-import Method from "@/app/method";
-import LogoGrid from "@/app/components/logo-grid";
-import TopServicesOverview from "@/app/components/top-services-overview";
-import AlgoCta from "@/app/components/algo-cta";
-import ServicesOverview from "@/app/components/services-overview";
-import StrategyCTA from "@/app/components/strategy-cta";
-import { TopPostsGrid } from "@/app/components/posts-grid";
+import Hero from '@/app/hero'
+import Method from '@/app/method'
+import LogoGrid from '@/app/components/logo-grid'
+import TopServicesOverview from '@/app/components/top-services-overview'
+import AlgoCta from '@/app/components/algo-cta'
+import ServicesOverview from '@/app/components/services-overview'
+import StrategyCTA from '@/app/components/strategy-cta'
+import { TopPostsGrid } from '@/app/components/posts-grid'
+import { Metadata } from 'next'
 
 // Lib imports
-import { logos } from "@/app/lib/logo";
-import { getFeaturedBlogPosts } from "@/app/lib/blog";
-import { topServices, servicesOverview } from "@/app/lib/services";
+import { logos } from '@/app/lib/logo'
+import { getFeaturedBlogPosts } from '@/app/lib/blog'
+import { topServices, servicesOverview } from '@/app/lib/services'
+
+export const metadata: Metadata = {
+  title: {
+    default: 'The Only SEO Agency You Will Ever Need | Blue Boost',
+    template: '%s | Blue Boost'
+  },
+  description:
+    'Looking for the top SEO agency? We can help improve your search engine optimization with our custom services. ✓ Get started!'
+}
 
 export default function Home() {
-  let featuredPosts = getFeaturedBlogPosts(3);
+  let featuredPosts = getFeaturedBlogPosts(3)
 
   return (
     <>
@@ -31,5 +41,5 @@ export default function Home() {
       <StrategyCTA />
       <TopPostsGrid posts={featuredPosts} />
     </>
-  );
+  )
 }
